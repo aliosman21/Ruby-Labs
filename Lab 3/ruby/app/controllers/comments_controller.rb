24cebf946670a786ba4f19846c24467de550cb4a
Ali Osman <amrali975@gmail.com>
2021-05-09 15:06:53 +0200

    Authenticated user

--- Lab 3/ruby/app/controllers/comments_controller.rb
+++ Lab 3/ruby/app/controllers/comments_controller.rb
@@ -1,19 +1,28 @@
 class CommentsController < ApplicationController
+
+  before_action :authenticate_user!
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    @comment.commenter = current_user.email
+    @comment.save
     redirect_to article_path(@article)
   end
  
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article)
+    if @comment.commenter == current_user.email
+      @comment.destroy
+      redirect_to article_path(@article)
+    else
+      flash[:fail] = "You did not create this comment"
+      redirect_to article_path(@article)
+    end
   end
  
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:body)
     end
 end
