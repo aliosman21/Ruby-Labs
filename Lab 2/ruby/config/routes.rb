065b02dc7e3ba14d52e01f1809ad89af80dd8078
Ali Osman <amrali975@gmail.com>
2021-05-09 13:00:47 +0200

    lab2 set

--- Lab 2/ruby/config/routes.rb
+++ Lab 2/ruby/config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
    resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get "/articles", to: "articles#index"
+   # get "/articles", to: "articles#index"
 end
