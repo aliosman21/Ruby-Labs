0429ed659280fd0dbbf440ec90ef674cde7345b4
Ali Osman <amrali975@gmail.com>
2021-05-09 13:58:10 +0200

    Added models and associated

--- Lab 3/ruby/app/controllers/articles_controller.rb
+++ Lab 3/ruby/app/controllers/articles_controller.rb
@@ -1,7 +1,58 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
+
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+
+
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    if @article.save
+    redirect_to @article
+    else
+    render 'new'
+    end
   end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+
 end
